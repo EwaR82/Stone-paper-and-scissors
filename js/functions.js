7038c09b7c65325dd30be8dee8c20bcd18685dac
Ewa Rudzinska <ewa.rudzinska@next-it.pl>
2020-06-07 18:48:38 +0200

    add new functions and changes in interface

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,65 @@
-function printMessage(msg) {
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+	function playerVsComputer(argPlayerMove) {
 
-function clearMessages() {
-	document.getElementById('messages').innerHTML = '';
-}
-
-
-function getMoveName(argMoveId) {
-	if (argMoveId == 1) {
-		return 'kamień';
-	} else if (argMoveId == 2) {
-		return 'papier';
-	} else if (argMoveId == 3) {
-		return 'nożyce';
-	} else {
-		return 'nieznany ruch';
+		document.getElementById('player-move').innerHTML = 'Gracz : ' + argPlayerMove;
+		randomNumber = Math.floor(Math.random() * 3);
+		computerMove = moveName[randomNumber];
+		document.getElementById('computer-move').innerHTML = 'Komputer : ' + computerMove;
+		whoWins = displayResult(computerMove, argPlayerMove);
+		if (whoWins === 'Wygrałeś!!') {
+			scorePlayer++;
+			document.getElementById('player-score').innerHTML = 'Punkty Gracz : ' + scorePlayer;
+		}
+		if (whoWins === 'Przegrałeś!!') {
+			scoreComputer++;
+			document.getElementById('computer-score').innerHTML = 'Punkty Komputer : ' + scoreComputer;
+		}
+		if (whoWins === 'Remis') {
+			scorePlayer++;
+			document.getElementById('player-score').innerHTML = 'Punkty Gracz : ' + scorePlayer;
+			scoreComputer++;
+			document.getElementById('computer-score').innerHTML = 'Punkty Komputer : ' + scoreComputer;
+		}
+		numberRound++;
+		setTimeout(function () {
+			document.getElementById('number-round').innerHTML = 'Runda : ' + numberRound;
+			document.getElementById('player-move').innerHTML = 'Gracz : Wybierz';
+			document.getElementById('computer-move').innerHTML = 'Komputer : losuje';
+			if (numberRound >= 6) {
+				endGame()
+			};
+		}, 2000);
 	}
-}
 
-function displayResult(argComputerMove, argPlayerMove) {
-	if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
-		(argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
-		(argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
-		return 'Wygrałeś!!';
-	} else if ((argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
-		(argComputerMove == 'nożyce' && argPlayerMove == 'papier') ||
-		(argComputerMove == 'kamień' && argPlayerMove == 'nożyce')) {
-		return 'Przegrałeś!!';
-	} else if ((argComputerMove == 'nieznany ruch' || argPlayerMove == 'nieznany ruch')) {
-		return 'Zły ruch!!!';
-	} else {
-		return 'Tym razem Remis';
+	function endGame() {
+		document.getElementById('number-round').innerHTML = 'Koniec Gry!!';
+		if (scorePlayer > scoreComputer) {
+			document.getElementById('player-move').innerHTML = 'Wygrałeś Grę!!!'
+			document.getElementById('computer-move').hidden = true;
+		} else if (scorePlayer < scoreComputer) {
+			document.getElementById('computer-move').innerHTML = 'Przegrałeś Grę!!!'
+			document.getElementById('player-move').hidden = true;
+		} else {
+			document.getElementById('player-move').innerHTML = 'Remis!!!'
+			document.getElementById('computer-move').innerHTML = 'Remis!!!'
+		}
+		document.getElementById('btn-start').hidden = true;
+		document.getElementById('btn-stone').hidden = true;
+		document.getElementById('btn-paper').hidden = true;
+		document.getElementById('btn-scissors').hidden = true;
+		document.getElementById('btn-new-game').hidden = false;
 	}
-}
+
+
+	function displayResult(argComputerMove, argPlayerMove) {
+		if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+			(argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
+			(argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
+			return 'Wygrałeś!!';
+		} else if ((argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
+			(argComputerMove == 'nożyce' && argPlayerMove == 'papier') ||
+			(argComputerMove == 'kamień' && argPlayerMove == 'nożyce')) {
+			return 'Przegrałeś!!';
+		} else {
+			return 'Remis';
+		}
+	}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,59 @@
-// player move
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-let playerMove = getMoveName(playerInput);
-printMessage('Twój ruch to: ' + playerMove);
+let scorePlayer = 0;
+let scoreComputer = 0;
+let moveName = ['kamień', 'papier', 'nożyce'];
+let playerMove;
+let computerMove;
+let randomNumber;
+let numberRound = 1;
 
-// computer move
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-printMessage('Mój ruch to: ' + computerMove);
+document.getElementById('btn-start').addEventListener('click', function () {
+  document.getElementById('number-round').hidden = false;
+  document.getElementById('btn-start').hidden = true;
+  document.getElementById('btn-stone').hidden = false;
+  document.getElementById('btn-paper').hidden = false;
+  document.getElementById('btn-scissors').hidden = false;
+  document.getElementById('player-move').hidden = false;
+  document.getElementById('computer-move').hidden = false;
+  document.getElementById('player-score').hidden = false;
+  document.getElementById('computer-score').hidden = false;
+});
 
-// result
-let whoWins = displayResult(computerMove, playerMove);
-printMessage('Wynik gry to: ' + whoWins);
+document.getElementById('btn-stone').addEventListener('click', function () {
+  if (numberRound <= 5) {
+    playerMove = 'kamień';
+    playerVsComputer(playerMove);
+  }
+});
+document.getElementById('btn-paper').addEventListener('click', function () {
+  if (numberRound <= 5) {
+    playerMove = 'papier';
+    playerVsComputer(playerMove);
+  }
+});
+document.getElementById('btn-scissors').addEventListener('click', function () {
+  if (numberRound <= 5) {
+    playerMove = 'nożyce';
+    playerVsComputer(playerMove);
+  }
+});
+
+document.getElementById('btn-new-game').addEventListener('click', function () {
+  scorePlayer = 0;
+  scoreComputer = 0;
+  numberRound = 1;
+  document.getElementById('number-round').hidden = false;
+  document.getElementById('btn-start').hidden = true;
+  document.getElementById('btn-stone').hidden = false;
+  document.getElementById('btn-paper').hidden = false;
+  document.getElementById('btn-scissors').hidden = false;
+  document.getElementById('player-move').hidden = false;
+  document.getElementById('computer-move').hidden = false;
+  document.getElementById('player-score').hidden = false;
+  document.getElementById('computer-score').hidden = false;
+  document.getElementById('btn-new-game').hidden = true;
+  document.getElementById('player-score').innerHTML = 'Punkty Gracz : 0';
+  document.getElementById('computer-score').innerHTML = 'Punkty Komputer : 0';
+  document.getElementById('number-round').innerHTML = 'Runda : 1';
+  document.getElementById('player-move').innerHTML = 'Gracz : Wybierz';
+  document.getElementById('computer-move').innerHTML = 'Komputer : losuje';
+});
